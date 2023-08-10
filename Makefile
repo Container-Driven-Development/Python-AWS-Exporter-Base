8cdf2072696a0d644e95ffaeea165b7f586b09d5
Mario Vejlupek <mario@vejlupek.cz>
2023-08-10 10:52:05 +0200

    feat(helm): :sparkles: add METRICS_SCRAPE_INTERVAL and env support
    Signed-off-by: Mario Vejlupek <mario@vejlupek.cz>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 test:
-	helm upgrade --install --debug -n customer-monitoring-system --set-file script_py=./tmp/script.py -f ./tmp/values.test.yaml s3-metrics ./helm
+	helm upgrade --install --debug -n customer-monitoring-system --set-file script_py=./examples/s3-metrics.py -f ./tmp/values.test.yaml s3-metrics ./helm
 
 template:
-	helm template --debug --output-dir=./tmp/ -n customer-monitoring-system --set-file script_py=./tmp/script.py -f ./tmp/values.test.yaml s3-metrics ./helm
+	helm template --debug --output-dir=./tmp/ -n customer-monitoring-system --set-file script_py=./examples/s3-metrics.py -f ./tmp/values.test.yaml s3-metrics ./helm
 
---
+++ examples/s3-metrics.py
@@ -0,0 +1,55 @@
+import os
+import boto3
+from prometheus_client import start_http_server, Gauge
+import time
+import logging
+from pythonjsonlogger import jsonlogger
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger()
+
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
+logger.info({"state": "starting"})
+
+# Create a Gauge metric for Prometheus
+s3_bucket_folder_size = Gauge('s3_bucket_folder_size', 'Size of the S3 bucket folders', ['bucket', 'folder'])
+
+def get_s3_bucket_folder_sizes(bucket_name):
+    logger.info({"state": "processing", "bucket": bucket_name, "message": "Getting folder sizes"})
+    s3 = boto3.client('s3')
+    paginator = s3.get_paginator('list_objects_v2')
+
+    folder_sizes = {}
+
+    for page in paginator.paginate(Bucket=bucket_name, Delimiter='/'):
+        for prefix in page.get('CommonPrefixes', []):
+            folder = prefix['Prefix']
+            size = 0
+            for obj in s3.list_objects_v2(Bucket=bucket_name, Prefix=folder)['Contents']:
+                size += obj['Size']
+            folder_sizes[folder.rstrip('/')] = size
+
+    return folder_sizes
+
+def update_metrics(bucket_names):
+    for bucket in bucket_names:
+        folder_sizes = get_s3_bucket_folder_sizes(bucket)
+        for folder, size in folder_sizes.items():
+            s3_bucket_folder_size.labels(bucket=bucket, folder=folder).set(size)
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+
+    # List of bucket names
+    buckets = os.environ.get('S3_BUCKET_NAMES').split(',')
+
+    # Update metrics every minute
+    while True:
+        update_metrics(buckets)
+        time.sleep(60)
